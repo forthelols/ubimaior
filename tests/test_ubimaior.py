501df8d06eca3022ed36f1d77a7ece492c1fcb2e
alalazo <massimiliano.culpo@googlemail.com>
2017-12-11 09:56:54 +0100

    MergedSequence: first complete implementation with tests

--- tests/test_ubimaior.py
+++ tests/test_ubimaior.py
@@ -9,17 +9,313 @@ import pytest
 from ubimaior import ubimaior
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+@pytest.fixture()
+def sequence():
+    """An instance of a merged sequence """
+    return ubimaior.MergedSequence([
+        [1, 2, 3],
+        ['a', 'b', 'c'],
+        [False, True, None],
+        [1, 'a', False]
+    ])
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+class TestMergedMapping(object):
+    def test_errors_on_init(self):
+        """Tests that errors occurring when constructing an
+        instance are handled in a proper way
+        """
+
+        # not a list
+        with pytest.raises(TypeError) as excinfo:
+            ubimaior.MergedMapping('not_the_correct_type')
+        assert '"mappings" should be a list' in str(excinfo.value)
+
+        # items are not of the correct type
+        with pytest.raises(TypeError) as excinfo:
+            ubimaior.MergedMapping([1, 2, 3])
+        assert 'items in "mappings" should be' in str(excinfo.value)
+
+    def test_reading_non_container_types(self):
+        """Tests that reading non-container types from a MergedMapping
+        behaves as designed.
+        """
+
+        highest_priority = {
+            'foo': 1
+        }
+
+        middle_priority = {
+            'foo': 6,  # type(foo) is always an int
+            'bar': 'this_is_bar'
+        }
+
+        lowest_priority = {
+            'bar': '4',  # type(bar) is always a string
+            'baz': False
+        }
+
+        merged = ubimaior.MergedMapping([
+            ('highest', highest_priority),
+            ('middle', middle_priority),
+            ('lowest', lowest_priority)
+        ])
+
+        assert merged['foo'] == 1
+        assert merged['bar'] == 'this_is_bar'
+        assert merged['baz'] is False
+
+        with pytest.raises(KeyError):
+            merged['this_key_does_not_exit']
+
+    def test_errors_when_reading(self):
+        """Tests all the errors that may happen when reading from a mapping."""
+        # Type mismatch on a key
+        merged = ubimaior.MergedMapping([
+            ('high', {'foo': 1}),
+            ('low', {'foo': 'bar'})
+        ])
+
+        msg = 'expected TypeError, because a str is overridden with an int'
+        with pytest.raises(TypeError, message=msg) as excinfo:
+            merged['foo']
+        assert 'type mismatch for key' in str(excinfo.value)
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+class TestMergedSequence(object):
+    def test_errors_on_init(self):
+        """Tests that errors occurring when constructing an
+        instance are handled in a proper way
+        """
+        # not a list
+        with pytest.raises(TypeError) as excinfo:
+            ubimaior.MergedSequence('not_the_correct_type')
+        assert '"sequences" should be a list' in str(excinfo.value)
+
+        # items are not of the correct type
+        with pytest.raises(TypeError) as excinfo:
+            ubimaior.MergedSequence([1, 2, 3])
+        assert 'items in "sequences" should be' in str(excinfo.value)
+
+    def test_getting_items(self, sequence):
+        """Tests that retrieving elements from the merged list
+        works as expected.
+        """
+
+        # Length is the sum of the lengths of items
+        assert len(sequence) == 12
+
+        # Getting items should work like in built-in lists
+        assert sequence[:3] == [1, 2, 3]
+        assert sequence[:4] == [1, 2, 3, 'a']
+        assert sequence[::2] == [1, 3, 'b', False, None, 'a']
+        assert sequence[0] == 1
+        assert sequence[6] is False
+        assert sequence[11] == sequence[-1]
+
+        assert list(reversed(sequence))[:3] == [False, 'a', 1]
+
+        with pytest.raises(IndexError) as excinfo:
+            sequence[12]
+        assert 'list index out of range' in str(excinfo.value)
+
+        with pytest.raises(TypeError) as excinfo:
+            sequence['a']
+        assert 'list indices must be integers or slices' in str(excinfo.value)
+
+    def test_modifying_mutable_items(self):
+        """Tests the that the modification semantic is the same as
+        built-in lists.
+        """
+
+        lists = [[1, 2, 3], ['a', 'b', 'c']]
+        dicts = [{1: 'foo'}]
+
+        m = ubimaior.MergedSequence([lists, dicts])
+
+        assert len(m) == 3
+
+        m[0].append(4)
+        assert lists[0] == [1, 2, 3, 4]
+
+    def test_setting_single_items(self, sequence):
+
+        originals = sequence.sequences[:]
+
+        sequence[1] = 'd'
+        assert sequence[1] == 'd'
+        assert originals[0][1] == 'd'
+
+        sequence[4] = 'z'
+        assert sequence[4] == 'z'
+        assert originals[1][1] == 'z'
+
+        sequence[-1] = 'z'
+        assert sequence[-1] == 'z'
+        assert originals[3][2] == 'z'
+
+    def test_error_setting_slices(self, sequence):
+        with pytest.raises(TypeError) as excinfo:
+            sequence[0:] = 1
+        assert 'can only assign an iterable' in str(excinfo.value)
+
+        with pytest.raises(ValueError) as excinfo:
+            sequence[-4:-8:-1] = [1, 2, 3, 4, 5]
+        assert 'attempt to use an extended slice with step' in str(excinfo.value)
+
+    @pytest.mark.parametrize('sl,vl,expected_components', [
+        (slice(1, 7), [2, 3, 4, 5, 6, 7, 8], [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, True, None],
+            [1, 'a', False]
+        ]),
+        (slice(11, 5), [101, 102], [
+            [1, 2, 3],
+            ['a', 'b', 'c'],
+            [False, True, None],
+            [1, 'a', 101, 102, False]
+        ]),
+        (slice(7, 4), [101, 102], [
+            [1, 2, 3],
+            ['a', 'b', 'c'],
+            [False, 101, 102, True, None],
+            [1, 'a', False]
+        ]),
+        (slice(-8, -4), [1, 2, 3, 4, 5], [
+            [1, 2, 3],
+            ['a', 1, 2],
+            [3, 4, 5, None],
+            [1, 'a', False]
+        ]),
+        # (slice(None, None, 2), [101, 102, 103, 104, 105, 106], [
+        #     [101, 2, 102],
+        #     ['a', 103, 'c'],
+        #     [104, True, 105],
+        #     [1, 106, False]
+        # ]),
+        # (slice(-4, -8, -1), [1, 2, 3, 4], [
+        #     [1, 2, 3],
+        #     ['a', 'b', 4],
+        #     [3, 1, 1],
+        #     [1, 'a', False]
+        # ]),
+    ])
+    def test_setting_slices(self, sequence, sl, vl, expected_components):
+        originals = sequence.sequences[:]
+
+        # Check that we behave like a list
+        normal_list = list(sequence)
+        normal_list[sl] = vl
+        sequence[sl] = vl
+
+        assert list(sequence) == normal_list
+
+        # Check that the components are modified as we expect
+        for component, expected in zip(originals, expected_components):
+            assert component == expected
+
+    def test_deleting_single_items(self, sequence):
+
+        originals = sequence.sequences[:]
+
+        with pytest.raises(IndexError):
+            del sequence[20]
+
+        assert len(originals[1]) == 3
+        del sequence[4]
+        assert len(originals[1]) == 2
+        assert originals[1] == ['a', 'c']
+
+        assert len(originals[3]) == 3
+        del sequence[-2]
+        assert len(originals[3]) == 2
+        assert originals[3] == [1, False]
+
+    @pytest.mark.parametrize('sl,expected_components', [
+        (slice(1, 7), [
+            [1],
+            [],
+            [True, None],
+            [1, 'a', False]
+        ]),
+        (slice(11, 5), [
+            [1, 2, 3],
+            ['a', 'b', 'c'],
+            [False, True, None],
+            [1, 'a', False]
+        ]),
+        (slice(-8, -4), [
+            [1, 2, 3],
+            ['a'],
+            [None],
+            [1, 'a', False]
+        ]),
+    ])
+    def test_deleting_slices(self, sequence, sl, expected_components):
+        originals = sequence.sequences[:]
+
+        # Check that we behave like a list
+        normal_list = list(sequence)
+        del normal_list[sl]
+        del sequence[sl]
+
+        assert list(sequence) == normal_list
+
+        # Check that the components are modified as we expect
+        for component, expected in zip(originals, expected_components):
+            assert component == expected
+
+    @pytest.mark.parametrize('idx,vl,expected_components', [
+        (0, 101, [
+            [101, 1, 2, 3],
+            ['a', 'b', 'c'],
+            [False, True, None],
+            [1, 'a', False]
+        ]),
+        (12, 101, [
+            [1, 2, 3],
+            ['a', 'b', 'c'],
+            [False, True, None],
+            [1, 'a', False, 101]
+        ]),
+        (26, 101, [
+            [1, 2, 3],
+            ['a', 'b', 'c'],
+            [False, True, None],
+            [1, 'a', False, 101]
+        ]),
+        # Testing negative index
+        (-1, 101, [
+            [1, 2, 3],
+            ['a', 'b', 'c'],
+            [False, True, None],
+            [1, 'a', 101, False]
+        ]),
+        (-25, 101, [
+            [101, 1, 2, 3],
+            ['a', 'b', 'c'],
+            [False, True, None],
+            [1, 'a', False]
+        ]),
+    ])
+    def test_insertion(self, sequence, idx, vl, expected_components):
+        originals = sequence.sequences[:]
+
+        # Check that we behave like a list
+        normal_list = list(sequence)
+        normal_list.insert(idx, vl)
+        sequence.insert(idx, vl)
+
+        assert list(sequence) == normal_list
+
+        # Check that the components are modified as we expect
+        for component, expected in zip(originals, expected_components):
+            assert component == expected
+
+    def test_insertion_errors(self, sequence):
+
+        with pytest.raises(TypeError) as excinfo:
+            sequence.insert('a', 101)
+        msg = str(excinfo.value)
+        assert '\'str\' object cannot be interpreted as an integer' == msg
--- ubimaior/ubimaior.py
+++ ubimaior/ubimaior.py
@@ -1,21 +1,276 @@
 # -*- coding: utf-8 -*-
 
-import collections.abc
+import collections
+import itertools
+
+try:
+    from collections.abc import MutableMapping, MutableSequence, Iterable
+except ImportError:
+    from collections import MutableMapping, MutableSequence, Iterable
 
 
-class MergedMapping(collections.abc.MutableMapping):
+def _is_tuple_str_mapping(obj):
+    """Predicate that returns True if object is a tuple of length 2,
+    composed of a string and a mapping.
+
+    Args:
+        obj (object): object to be inspected
+
+    Returns: True or False
+    """
+    return isinstance(obj, tuple) and len(obj) == 2 and \
+        isinstance(obj[0], str) and isinstance(obj[1], MutableMapping)
+
+
+class MergedMapping(MutableMapping):
     """Shows a list of mappings with different levels of priority as
     they were a single one.
 
     TODO: write something about merging and write rules
+
+    FIXME: Is this implementable in terms of collections.ChainMap?
     """
+    def __init__(self, mappings):
+        # Check the type of mappings, which should be a list of
+        # (str, Mapping) tuples
+        if not isinstance(mappings, list):
+            msg = '"mappings" should be a list of tuples'
+            raise TypeError(msg)
+
+        # Check that the items in the list have the correct type
+        if any(not _is_tuple_str_mapping(x) for x in mappings):
+            msg = 'items in "mappings" should be (str, Mapping)' \
+                  ' tuples of length 2'
+            raise TypeError(msg)
+
+        #: Ordered dict that contains the priority list
+        #: of mappings
+        self.mappings = collections.OrderedDict(mappings)
+
+    def __getitem__(self, item):
+        # TODO: currently doesn't handle containers
+
+        # If the key is not there, mimic built-in dict
+        if not any(item in x for x in self.mappings.values()):
+            raise KeyError(item)
+
+        # If the key is present in multiple mappings, and holds
+        # instances of different types raise a TypeError
+        types_for_mapping = {
+            k: type(v[item]) for k, v in self.mappings.items() if item in v
+        }
+
+        if len(set(types_for_mapping.values())) > 1:
+            # FIXME: Should we add additional checks to see if multiple types
+            # FIXME: are anyhow all Sequence or Mapping?
+            msg = 'type mismatch for key "{0}".'
+            msg += ' Overridden keys need to be of the same type.'
+            raise TypeError(msg.format(item))
+
+        for k, v in self.mappings.items():
+            # Non-container items
+            if item in v:
+                return self.mappings[k][item]
+
+    def __setitem__(self, key, value):
+        pass
+
+    def __delitem__(self, key):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __len__(self):
+        pass
 
 
-class MergedSequence(collections.abc.MutableSequence):
+class MergedSequence(MutableSequence):
     """Shows a list of sequences with different levels of priority as
     they were a single one.
 
     TODO: write something about merging and write rules
     """
+    def __init__(self, sequences):
+        # Check that sequences is a list of sequences
+        if not isinstance(sequences, list):
+            msg = '"sequences" should be a list of sequences'
+            raise TypeError(msg)
 
+        # Check that the items in the list have the correct type
+        if any(not isinstance(x, MutableSequence) for x in sequences):
+            msg = 'items in "sequences" should be MutableSequences'
+            raise TypeError(msg)
 
+        self.sequences = sequences
+
+    def __getitem__(self, idx):
+        self._raise_if_not_slice_or_integer(idx)
+
+        # Handle slices
+        if isinstance(idx, slice):
+            iterator = itertools.chain(*self.sequences)
+            return list(
+                itertools.islice(iterator, idx.start, idx.stop, idx.step)
+            )
+
+        # Handle integers
+        item_idx, idx = self._return_item_and_index(idx)
+        return self.sequences[item_idx][idx]
+
+    def __len__(self):
+        return sum([len(x) for x in self.sequences])
+
+    def __setitem__(self, idx, value):
+        self._raise_if_not_slice_or_integer(idx)
+
+        # Mimic built-in list for this case
+        if isinstance(idx, slice) and not isinstance(value, Iterable):
+            raise TypeError('can only assign an iterable')
+
+        # Handle slices
+        if isinstance(idx, slice):
+            # TODO: extend to slices of stride different from 1
+            if isinstance(idx.step, int) and idx.step != 1:
+                msg = 'attempt to use an extended slice with step {0}'
+                msg += ' [Only step == 1 is allowed]'
+                raise ValueError(msg.format(idx.step))
+
+            slices = self._split_slice(idx)
+            values = self._split_value(value, slices)
+            for x, sl, vl in zip(self.sequences, slices, values):
+                x[sl] = vl
+            return
+
+        # Handle integers
+        item_idx, idx = self._return_item_and_index(idx)
+        self.sequences[item_idx][idx] = value
+
+    def __delitem__(self, idx):
+        self._raise_if_not_slice_or_integer(idx)
+
+        # Handle slices
+        if isinstance(idx, slice):
+            slices = self._split_slice(idx)
+            for x, sl in zip(self.sequences, slices):
+                del x[sl]
+            return
+
+        # Handle integers
+        item_idx, idx = self._return_item_and_index(idx)
+        del self.sequences[item_idx][idx]
+
+    def insert(self, index, value):
+        if not isinstance(index, int):
+            msg = '\'{0}\' object cannot be interpreted as an integer'
+            raise TypeError(msg.format(type(index).__name__))
+
+        try:
+            item_idx, index = self._return_item_and_index(index)
+            self.sequences[item_idx].insert(index, value)
+        except IndexError:
+            # An IndexError means we are outside the bounds of
+            # the current list. In this case built-in lists insert
+            # the value as first or last element, depending in which
+            # direction we went out of bounds
+            if index > 0:
+                self.sequences[-1].append(value)
+            else:
+                self.sequences[0].insert(0, value)
+
+    def _raise_if_not_slice_or_integer(self, item):
+        """Raise a TypeError if the argument is not a slice or an integer.
+
+        Args:
+            item: object to be checked
+
+        Raises:
+            TypeError: when ``not isinstance(item, (slice, int))``
+        """
+        if not isinstance(item, (slice, int)):
+            msg = 'list indices must be integers or slices, not str'
+            raise TypeError(msg)
+
+    def _return_item_and_index(self, idx):
+        """Given an index that refers to the merged list, return the index of
+        the item in ``sequences`` that holds the item and its local index.
+
+        Args:
+            idx (int): index in the merged list
+
+        Returns:
+            item_idx, idx (int): index of the sequence and of the item
+                in the sequence
+
+        Raises:
+            IndexError: if ``idx`` is out of range
+        """
+        idx = len(self) + idx if idx < 0 else idx
+
+        for ii, x in enumerate(self.sequences):
+            if idx < len(x):
+                return ii, idx
+            idx -= len(x)
+
+        raise IndexError('list index out of range')
+
+    def _split_slice(self, idx):
+        """Given a slice for the merged list, returns a list of slices for
+        the underlying items.
+
+        Args:
+            idx (slice): slice referring to the merged list
+
+        Returns:
+            a list of slices associated with the managed items
+        """
+        slices = []
+
+        # Normalize to this length
+        idx = slice(*idx.indices(len(self)))
+
+        for x in self.sequences:
+            def next_slice(sl):
+                start = max(sl.start - len(x), 0)
+                stop = max(sl.stop - len(x), 0)
+                return slice(start, stop, sl.step)
+
+            local_indices = idx.indices(len(x))
+            slices.append(slice(*local_indices))
+            idx = next_slice(idx)
+
+        return slices
+
+    def _split_value(self, value, slices):
+        """Split a list of values meant for the merged list, to components
+        associated with each underlying item.
+
+        Args:
+            value (list of values): items to be assigned to the merged list
+            slices (list of slices): list of slices returned by
+                ``_split_slice``
+
+        Returns:
+            components associated with the underlying list
+        """
+        values = []
+
+        for sl in slices:
+            length = len(range(sl.start, sl.stop, sl.step))
+            local_value, value = value[:length], value[length:]
+            values.append(local_value)
+
+        # If I still have items in value I need to append
+        # them in the proper place
+        if len(value) > 0:
+            try:
+                [x for x in values if len(x)][-1].extend(value)
+            except IndexError:
+                # All the slices are empty
+                next(
+                    (v for sl, v, x in zip(slices, values, self.sequences)
+                     if sl.start < len(x)),
+                    values[-1]
+                ).extend(value)
+
+        return values

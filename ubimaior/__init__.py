c4393537e6067bfe0e9261770856cdfd6497a1a6
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2018-12-10 13:51:52 +0100

    Fixed cyclic dependency

--- ubimaior/__init__.py
+++ ubimaior/__init__.py
@@ -6,10 +6,10 @@ __author__ = """Massimiliano Culpo"""
 __email__ = 'massimiliano.culpo@gmail.com'
 __version__ = '0.1.0'
 
-from .formats import formatter
 from .mappings import OverridableMapping, MergedMapping
 from .sequences import MergedMutableSequence, MergedSequence
-from .configurations import load, dump
+from .configurations import load, dump  # pylint: disable=cyclic-import
+from .formats import formatter  # pylint: disable=cyclic-import
 
 __all__ = [
     'formatter',
